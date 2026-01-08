101000e21f2e9d44257e31a6f3ac4f57bb60ed74
hilaryoseiokrah <hilary.okrah@ashesi.edu.gh>
2026-01-08 15:41:14 +0000

    Updated

--- react-trial/src/components/ListGroup.tsx
+++ react-trial/src/components/ListGroup.tsx
@@ -1,16 +1,25 @@
 import { Fragment } from "react";
+import { MouseEvent } from "react";
 
 function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+  let  items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleClick = (event: MouseEvent) => console.log(event) ;
+  ;
 
   return (
     <Fragment>
       <h1>Groceries List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>

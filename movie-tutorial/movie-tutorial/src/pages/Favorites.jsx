7c6757c3a0fdd7b8701394e88025c55dbc1d26e2
hilaryoseiokrah <hilary.okrah@ashesi.edu.gh>
2026-01-09 22:38:42 +0000

    Installed react-router-dom and added a Favorites

---
+++ movie-tutorial/movie-tutorial/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorite Movies Yet</h2>
+      <p>Add some movies to your favorites to see them here.</p>
+    </div>
+  );
+}
+export default Favorite;

1b8376f8ff5c280fc4a03dce56d68dad19ea3a77
hilaryoseiokrah <hilary.okrah@ashesi.edu.gh>
2026-01-09 22:02:25 +0000

    Add movie card component

--- movie-tutorial/movie-tutorial/src/App.jsx
+++ movie-tutorial/movie-tutorial/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import MovieCard from "./components/MovieCard";
 function App() {
-  const [count, setCount] = useState(0)
+  const movieNumber = 2;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "Hilary Adv", release_date: "2025" }} />
+      ) : (
+        <MovieCard movie={{ title: "Joe Adv", release_date: "2025" }} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

76c7518c766c733c6942825fe172aed7e7eebb4b
hilaryoseiokrah <hilary.okrah@ashesi.edu.gh>
2026-01-10 00:26:59 +0000

    Favorites Logic Added

--- movie-tutorial/movie-tutorial/src/App.jsx
+++ movie-tutorial/movie-tutorial/src/App.jsx
@@ -1,20 +1,22 @@
 import "./css/App.css";
-import "./css/NavBar.css";
 import React from "react";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
 function App() {
   return (
     <div>
-      <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
+      <MovieProvider>
+        <NavBar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </MovieProvider>
     </div>
   );
 }
